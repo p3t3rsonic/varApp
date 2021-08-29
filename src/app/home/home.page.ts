739e3b3833113c6ef2150b8730e68c5eb5107c0a
Pedro Passarin <pedro_passarin@hotmail.com>
2021-08-29 16:35:40 -0300

    alterações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+public mensagem = "Olá mundo";
+public nome: string;
+public idade: number;
+public ativo: boolean;
 
+public situacao: any;
+
+  constructor() {
+
+    // alert ("Olá mundo!");
+
+    // alert ("Mensagem: " + this.mensagem);
+
+    //console.log ("Mensagem: " + this.mensagem);
+
+    this.mensagem = "Olá pessoal";
+    this.situacao = "teste";
+
+    // this.idade = 52;
+  }
+
+public msg(){
+
+  this.mensagem = this.nome + ", você informou que sua idade é, " + this.idade + ", anos.";
+  if (this.idade >= 18){
+    this.situacao = "Você é maior de idade."
+  }else{
+      this.situacao = "Você é menor de idade."
+    }
+
+  }
 }
+
+
